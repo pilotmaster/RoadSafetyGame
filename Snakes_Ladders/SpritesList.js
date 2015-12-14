59891f7377175b6ac493161847724637606ad1e2
pilotmaster - Kyle Hobdey <pilot_master@hotmail.co.uk>
2015-12-14 15:24:09 +0000

    The board displays when run

---
+++ Snakes_Ladders/SpritesList.js
@@ -0,0 +1,37 @@
+// Declare a list of sprite variables
+var
+spriteBoard;
+
+
+// Declare the Sprite class
+function CSprite(image, posX, posY, width, height)
+{
+	// Attributes
+	this.mImage = image;
+	this.mPosX = posX;
+	this.mPosY = posY;
+	this.mWidth = width;
+	this.mHeight = height;
+	
+	// Methods
+	this.Draw = function(context, posX, posY)
+	{
+		// Draw the sprite at the provided location using the saved data
+		context.drawImage(this.mImage, this.mPosX, this.mPosY, this.mWidth, this.mHeight, posX, posY, this.mWidth, this.mHeight);
+	}
+}
+
+
+// Function to initialise all the sprites
+function LoadSprites(board)
+{
+	// There are two boards available: Road Safety and Sustainable Development
+	// Determine which one has been chosen
+	if (board == "RS")
+	{
+		var boardImage = new Image();
+		boardImage.src = "res/RoadSafetyBoard.png";
+		spriteBoard = new CSprite(boardImage, 0, 0, 946, 946);
+		alert(spriteBoard.mWidth + " " + spriteBoard.mHeight);
+	}
+}
